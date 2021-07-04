73e28db21219fa7ce8ddf3ef42b5e7f734fade4c
HelluBoiz <86915204+HelluBoiz@users.noreply.github.com>
2021-07-04 20:04:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'Nhii ahhhh !',
+    introTitle: 'Nhii ah !',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Game khongg =)))',
-    desc: 'Game diiiii :33  ',
-    btnYes: 'Ukii , dzoooooo ',
-    btnNo: 'Khôngg =))) 3',
-    question:'Ăn j chx mà Game rùi :>> ',
-    btnReply: 'Gửi cho Phongzzz ',
-    reply: 'Dzoo Gameee ik :) ',
-    mess: 'Uh , mình bt mà :33',
-    messDesc: 'Dzôoo chs giờ nàaa :)).',
-    btnAccept: 'Okiiiii lunn :33',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Game khongg :))',
+    desc: 'Game diiiiiii :33 ',
+    btnYes: 'Ok lun nàa ',
+    btnNo: 'Khôngg =)))) :3',
+    question:'Ăn j chx mà Game rùi iii',
+    btnReply: 'Gửi cho Phongzz ',
+    reply: 'Dzoo gameeeee',
+    mess: 'Uh , bít gòi , Dzoo ',
+    messDesc: 'Dzoo h nha :)).',
+    btnAccept: 'Okiiiii lunn :33 ',
+    messLink: 'https://www.facebook.com/nhaphong.nguyenduy.3/
